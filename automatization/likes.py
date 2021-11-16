da5c6cfb9cfb91173d20be556ecbf0025d3ea405
penguin_PC <djerrylinux@gmail.com>
2021-11-16 12:16:17 +0000

    Был исправлен xpath к popup уведомлению.

--- automatization/likes.py
+++ automatization/likes.py
@@ -46,9 +46,6 @@ class YoutubeLiker:
         self.driver.get(self.url)
 
     def login(self, mail, passwd):
-        body = self.driver.find_elements_by_tag_name('input')
-        for b in body:
-            print(b.text)
         mail_field = self.wait.until(ec.presence_of_element_located((By.ID, 'identifierId')))
         mail_field.click()
         mail_field.send_keys(mail + Keys.ENTER)
@@ -56,6 +53,9 @@ class YoutubeLiker:
         # submit_first.click()
 
         sleep(3)
+        spans = self.driver.find_elements_by_tag_name('span')
+        for span in spans:
+            print(span.text)
         passwd_field = self.wait.until(ec.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input')))
         passwd_field.click()
         passwd_field.send_keys(passwd)
